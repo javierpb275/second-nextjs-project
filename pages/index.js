5b6d01cab046b69e578893daf22efd1444b99e1a
javierpb275 <javierpb275@gmail.com>
2021-09-04 18:43:07 +0200

    formatted files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import fs from 'fs/promises';
-import path from 'path';
+import fs from "fs/promises";
+import path from "path";
 
 const HomePage = (props) => {
   const { products } = props;
@@ -13,13 +13,13 @@ const HomePage = (props) => {
 };
 
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
   return {
     props: {
-      products: data.products
+      products: data.products,
     },
   };
 }
