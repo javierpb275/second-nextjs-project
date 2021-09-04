b6224b1b419c2ef2979ba95ccb7c5378c19505e0
javierpb275 <javierpb275@gmail.com>
2021-09-04 18:26:20 +0200

    Adding getStaticProps To Pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
 const HomePage = (props) => {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+};
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        {
+          id: "p1",
+          title: "Product 1",
+        },
+        {
+          id: "p2",
+          title: "Product 2",
+        },
+        {
+          id: "p3",
+          title: "Product 3",
+        },
+      ],
+    },
+  };
 }
 
 export default HomePage;
