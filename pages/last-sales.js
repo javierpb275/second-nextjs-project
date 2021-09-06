343326c547ec0ab05d35ebcdb855217388a0b451
javierpb275 <javierpb275@gmail.com>
2021-09-06 19:16:20 +0200

    Using the useSWR Nextjs hook for fetching the data.
    intalled swr:
    
    The name “SWR” is derived from stale-while-revalidate, a HTTP cache invalidation strategy popularized by HTTP RFC 5861. SWR is a strategy to first return the data from cache (stale), then send the fetch request (revalidate), and finally come with the up-to-date data.
    
    With SWR, components will get a stream of data updates constantly and automatically.
    And the UI will be always fast and reactive.

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,10 +1,31 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const LastSalesPage = (props) => {
-  const [sales, setSales] = useState(props.sales);
-  const [isLoading, setIsLoading] = useState(false);
+  const [sales, setSales] = useState();
+  //const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-8044b-default-rtdb.firebaseio.com/sales.json"
+  );
 
   useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  /*   useEffect(() => {
     setIsLoading(true);
     fetch("https://nextjs-course-8044b-default-rtdb.firebaseio.com/sales.json")
       .then((response) => response.json())
@@ -22,14 +43,14 @@ const LastSalesPage = (props) => {
         setSales(transformedSales);
         setIsLoading(false);
       });
-  }, []);
+  }, []); */
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (error) {
+    return <p>Failed to load</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
