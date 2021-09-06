3d2d3f155f6b41e643e896c490ac22a984836be9
javierpb275 <javierpb275@gmail.com>
2021-09-06 18:27:44 +0200

    Implementing client-side data fetching

---
+++ pages/last-sales.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+const LastSalesPage = (props) => {
+  const [sales, setSales] = useState(props.sales);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://nextjs-course-8044b-default-rtdb.firebaseio.com/sales.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const transformedSales = [];
+
+        for (const key in data) {
+          transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
+        }
+
+        setSales(transformedSales);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!sales) {
+    return <p>No data yet</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default LastSalesPage;
